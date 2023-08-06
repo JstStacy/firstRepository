906cc107760def20e2ea3a21751bde3a5e82936a
Anastasiia Taran <anastasiataran96@gmail.com>
2023-08-06 16:04:04 +0300

    Aded fixtures

---
+++ tests/api/test_api.py
@@ -0,0 +1,31 @@
+import pytest
+
+class User:
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Anastasiia"
+        self.second_name = "Taran"
+
+    def remove(self):
+        self = " "
+        self.second_name = " "
+
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+   
+    user.remove()
+
+def test_change_name(user):
+    assert user.name == 'Anastasiia'
+
+
+def test_change_second_name(user):
+    assert user.second_name == 'Taran'
