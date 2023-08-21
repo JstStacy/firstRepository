aa8d7bfb7ac4f19ebce4eab75637434550225e81
Anastasiia Taran <anastasiataran96@gmail.com>
2023-08-21 10:15:35 +0300

    Added API testing

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.github import GitHub
 
 
 class User:
@@ -25,6 +26,12 @@ def user():
 
     user.remove()
 
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
+    
 
 
     
---
+++ tests/api/test_github_api.py
@@ -0,0 +1,54 @@
+import pytest
+import requests
+from modules.api.clients.github import GitHub
+
+
+@pytest.mark.api
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
+    assert user['login'] == 'defunkt'
+
+@pytest.mark.api
+def test_user_not_exists(github_api):
+    r = github_api.get_user('anastasiiataran')
+    assert r['message'] == 'Not Found'  
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 43
+    assert 'become-qa-auto' in r['items'][0]['name']
+   
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('anastasiiataran_repo_non_exist')
+    assert r['total_count'] == 0
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
+
+@pytest.mark.api
+def test_combined_status_for_a_specific_reference(github_api):
+    r = github_api.get_a_combined_status('JstStacy','firstRepository', 'c7cee272b4e5d938ca0d86210900aeb05e7853c5')
+    assert r['state'] == 'pending'
+
+@pytest.mark.api
+def test_combined_status_for_non_exicting_reference(github_api):
+    r = github_api.get_a_combined_status('JstStacy','firstRepository', 'ascee274e5d938ca0d86210900aeb05e7853c5')
+    assert r['message'] == 'Ref not found'
+
+@pytest.mark.api
+def test_combined_status_for_non_exicting_user(github_api):
+    r = github_api.get_a_combined_status('anastasiiataran','firstRepository', 'ascee274e5d938ca0d86210900aeb05e7853c5')
+    assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_emojis(github_api):
+    r = github_api.get_emojis()
+    assert r['100'] == 'https://github.githubassets.com/images/icons/emoji/unicode/1f4af.png?v8'
+
+
+
+
